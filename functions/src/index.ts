bab6ad013270f7f51d56b8e37cb1bebee4abf6e3
kazuya-m <kazuya.matsuo5@gmail.com>
2020-10-18 08:10:48 +0900

    create cloud function to add dataset

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,28 @@
 import * as functions from 'firebase-functions';
-import * as admin from "firebase-admin"; admin.initializeApp();
+import * as admin from "firebase-admin";
+// import { object } from 'firebase-functions/lib/providers/storage';
+// import { border } from '@material-ui/system';
+ admin.initializeApp();
 
 const db = admin.firestore();
 
 const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
   response.send({
     statusCode,
-    body: JSON.stringify(body);
+    body: JSON.stringify(body)
   })
 };
 
 //外部からcloud-function使う場合はexport
 export const addDataset = functions.https.onRequest( async(req: any, res: any) => {
-
+  if(req.method !== 'POST') {
+    sendResponse(res, 405, {error: 'Invalid Request'});
+  } else {
+    const dataset = req.body;
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key];
+      await db.collection('questions').doc(key).set(data);
+    }
+    sendResponse(res, 200, {message: 'Successgulyy added dataset'});
+  }
 })
