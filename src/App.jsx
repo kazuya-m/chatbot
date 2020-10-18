97c43f8721230d3fe412407dcc20320e835cb59b
kazuya-m <kazuya.matsuo5@gmail.com>
2020-10-18 10:24:15 +0900

    get n setfirebase dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React, { createElement } from 'react';
-import defaultDataset from './dataset';
+import React from 'react';
 import './assets/style/style.css';
 import {AnswersList, Chats} from './components/index';
-import { ChatSharp } from '@material-ui/icons';
 import FromDialog from './components/forms/FormDialog';
-
+import {db} from './firebase/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,7 +11,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this);
@@ -72,7 +70,24 @@ export default class App extends React.Component {
     this.setState({open: false});
   }
 
+  initDataset = dataset => {
+    this.setState({dataset: dataset});
+
+  }
+
   componentDidMount() {
+    (async() => {
+      const dataset = this.state.dataset;
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id;
+          const data = doc.data()
+          dataset[id] = data;
+        })
+      });
+      this.initDataset(dataset);
+    })();
+
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId);
   }
@@ -98,7 +113,6 @@ export default class App extends React.Component {
           <FromDialog open={this.state.open} handleClickClose={this.handleClickClose} />
         </div>
       </section>
-
     );
   }
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjkw6fLn69Nj5oTH8_59JxM65LUcOr1-4",
+  authDomain: "chat-bot-c069a.firebaseapp.com",
+  databaseURL: "https://chat-bot-c069a.firebaseio.com",
+  projectId: "chat-bot-c069a",
+  storageBucket: "chat-bot-c069a.appspot.com",
+  messagingSenderId: "679847818858",
+  appId: "1:679847818858:web:abeebff9167d2ec058fc13",
+  measurementId: "G-B6DXYLR01Y"
+};
+
+export default firebaseConfig;
