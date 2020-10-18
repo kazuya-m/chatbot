57b0941547d4d353ce8389eea594aa4d7ae8811e
kazuya-m <kazuya.matsuo5@gmail.com>
2020-10-18 11:55:44 +0900

    some comment changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
-// import { object } from 'firebase-functions/lib/providers/storage';
-// import { border } from '@material-ui/system';
- admin.initializeApp();
+
+
+//Firebaseにデプロイする関数。外部から使用してFirestoreのデータ操作などを行う
+admin.initializeApp();
 
 const db = admin.firestore();
+
 
 const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
   response.send({
@@ -13,15 +15,17 @@ const sendResponse = (response: functions.Response, statusCode: number, body: an
   })
 };
 
-//外部からcloud-function使う場合はexport
+// 外部からcloud-function使う場合はexport
+// Dataset
 export const addDataset = functions.https.onRequest( async(req: any, res: any) => {
+  // POST以外は受け付けない
   if(req.method !== 'POST') {
     sendResponse(res, 405, {error: 'Invalid Request'});
   } else {
     const dataset = req.body;
     for (const key of Object.keys(dataset)) {
       const data = dataset[key];
-      await db.collection('questions').doc(key).set(data);
+      await db.collection('questions').doc(key).set(data); // Firestorのデータ構造。collection - document - data の階層
     }
     sendResponse(res, 200, {message: 'Successgulyy added dataset'});
   }
