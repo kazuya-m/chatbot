e1c16cd060031991a60b159166e7c624782a2ed7
kazuya-m <kazuya.matsuo5@gmail.com>
2020-12-05 21:21:26 +0900

    create skill-json-dataset and card component

---
+++ src/components/modal/MediaCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const MediaCard = props => {
+  const classes = useStyles();
+
+
+  return (
+    <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.exp}
+          </Typography>
+        </CardContent>
+    </Card>
+  );
+}
+
+
+export default MediaCard;
--- src/components/modal/Skills.jsx
+++ src/components/modal/Skills.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import MediaCard from './MediaCard';
+import skillSet from '../../assets/dataset/skillset.json'
 //import Icon from './Icon.js';
 
+
 const Skills = () => {
+  // JSONから渡ってくる文字列
+  const study = 'study';
+  const work = 'work';
+  const both = 'both';
+
+  // 表示用文字列
+  const studyToString = '自己学習';
+  const workToString = '業務経験';
+  const bothToString = '業務経験 / 自己学習';
+
+  // JSONから渡ってきた文字列を表示用に変更
+  Object.keys(skillSet).forEach(key => {
+    switch (skillSet[key].exp) {
+      case study:
+        skillSet[key].exp = studyToString;
+        break;
+      case work:
+        skillSet[key].exp = workToString;
+        break;
+      case both:
+        skillSet[key].exp = bothToString;
+        break;
+      default:
+        break;
+    }
+  });
+ 
   return (
-    <h2>its skills!</h2>
+    <div>
+      {Object.keys(skillSet).map(key => (
+        // <MediaCard />
+        <MediaCard key={key} name={skillSet[key].id} exp={skillSet[key].exp} />
+       ))}
+    </div>
   )
 
 
