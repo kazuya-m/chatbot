c7fb4add9f495ded3d2c605610ded64f762557dc
kazuya-m <kazuya.matsuo5@gmail.com>
2020-12-20 23:14:27 +0900

    add auth initialization

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
+import 'firebase/auth';
 import firebaseConfig from './config';
 
 firebase.initializeApp(firebaseConfig);
+
+// Auth
+export const auth = firebase.auth();
 
 // Firestore
 export const db = firebase.firestore();
